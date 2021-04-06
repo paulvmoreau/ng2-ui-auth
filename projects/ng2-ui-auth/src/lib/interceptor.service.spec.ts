beb9d759379cad004b3a9ef7fef1b6debe77e611
Paul Moreau <pmoreau@alienvault.com>
2021-04-06 13:53:28 -0500

    new build

--- projects/ng2-ui-auth/src/lib/interceptor.service.spec.ts
+++ projects/ng2-ui-auth/src/lib/interceptor.service.spec.ts
@@ -1,15 +1,15 @@
 import { TestBed, inject } from '@angular/core/testing';
 
-import { InterceptorService } from './interceptor.service';
+import { JwtInterceptor } from './interceptor.service';
 
 describe('InterceptorService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [InterceptorService]
+      providers: [JwtInterceptor]
     });
   });
 
-  it('should be created', inject([InterceptorService], (service: InterceptorService) => {
+  it('should be created', inject([JwtInterceptor], (service: JwtInterceptor) => {
     expect(service).toBeTruthy();
   }));
 });
