a61cb3b335789ad392d876c39ef96b19d447d80d
Paul Moreau <pmoreau@alienvault.com>
2022-08-19 11:08:56 -0500

    v10.0.8

--- dist/lib/lib/ng2-ui-auth-11.module.d.ts
+++
@@ -1,5 +0,0 @@
-import { ModuleWithProviders } from '@angular/core';
-import { IPartialConfigOptions } from './config-interfaces';
-export declare class Ng2UiAuth11Module {
-    static forRoot(configOptions?: IPartialConfigOptions, defaultJwtInterceptor?: boolean): ModuleWithProviders<any>;
-}
